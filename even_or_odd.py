f1bb2565e7eceb72883582c14096895dfd307262
picral <lusjoh518@gmail.com>
2016-11-06 20:45:58 -0600

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 20:26:16 2016
+
+@author: hg
+
+Even or Odd
+https://www.codewars.com/kata/even-or-odd/train/python
+
+"""
+
+def even_or_odd(number):    
+    if number % 2 == 0:
+        return 'Even'
+    return 'Odd'
+            
